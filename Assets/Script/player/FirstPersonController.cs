412bc642699e9d11afcfcc9b6bdd940ced6fd530
Sigma-sergay <niukin.s.s_ipz24@rcit.ukr.education>
2025-10-29 18:43:08 +0200

    o
    ogo

---
+++ Assets/Script/player/FirstPersonController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b87882a42561129c6203254a2a12b5be847a1513c557817f6bace4235696d783
+size 4206
---
+++ Assets/Script/scenes/DoorTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb733549a1f1f97c3e21f655cc02d2259b097f217c8021d7cfd5706ff4131e4b
+size 2317
