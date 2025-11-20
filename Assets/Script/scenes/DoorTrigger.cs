f0ae5ec7231779ff6cbcfc3340a0d52b8c9cf287
Bziton <erorteam231@gmail.com>
2025-11-20 21:49:46 +0200

    door_dead_respawn

--- Assets/Script/scenes/DoorTrigger.cs
+++ Assets/Script/scenes/DoorTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d02677ab899d6ea1a40e302b946e3979f0f69b9f66c5d88e2bd30a55790e8f03
-size 11839
+oid sha256:eef9f5ac627cc6c0e8d4af2f29d8277b24de4d825f5981bc684a03579d4fd89f
+size 10539
