5b3ac688f9e846b4fba6f3b956b836df96c7478d
Bziton <erorteam231@gmail.com>
2025-11-22 18:09:27 +0200

    death screen

--- Assets/Script/scenes/DoorTrigger.cs
+++ Assets/Script/scenes/DoorTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eef9f5ac627cc6c0e8d4af2f29d8277b24de4d825f5981bc684a03579d4fd89f
-size 10539
+oid sha256:e2e5632ec52a9b6b6ae65dc675abac14963acff42fd6b651c3dbc2ef751bab18
+size 11532
